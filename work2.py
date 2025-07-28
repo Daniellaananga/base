2147636dc17ddc26338a3fce219e8e52d1c01893
Daniellaananga <daniellasong06.@gmail.com>
2025-07-28 15:53:46 +0100

    learning how to manage python texts commit

---
+++ work2.py
@@ -0,0 +1,40 @@
+from colorama import Fore, Back, Style
+print(Fore.YELLOW + Back.RED +"For This Exercise You Will Be Asked Four Numbers Of Your Choice  " + Style.BRIGHT + Style.RESET_ALL)
+a=int (input("1:"))
+b=int (input("2:"))
+c=int (input("3:"))
+d=int (input("4:"))
+if a % 2 == 0:
+    print("this number is EVEN!")
+else:
+    print("this number is ODD!")
+    if b % 2 == 0:
+        print("this number is EVEN!")
+    else:
+          print("this number is ODD!")   
+if c % 2 == 0:
+        print("this number is EVEN!")
+else:
+    print("this number is ODD!") 
+if d % 2 == 0:
+        print("this number is EVEN!")
+else:
+    print("this number is ODD!") 
+    X=a+b+c+d
+    print("the sum X is :", X)
+    Y=a*b*c*d
+    print("the multiplication Y is :", Y)
+if a % 2 & b % 2 == 0:
+      T=a/b
+      print("the quotient T is :", T)
+else:
+      if a % 2 == 0:
+          print("this number is EVEN!")
+      else:
+              print("this number is ODD!")
+      if b % 2 == 0:
+            print("this number is EVEN!")
+      else:
+            print("this number is ODD! //n ")
+            print("INCOMPATIBLE!!!")
+
