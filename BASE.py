9a7dfaad2fcf6e8cf82deed36289b307cee3b5e2
Daniellaananga <daniellasong06.@gmail.com>
2025-07-28 13:21:29 +0100

    update on this page

--- BASE.py
+++ BASE.py
@@ -2,4 +2,4 @@ print("enter two numbers :")
 a=int (input("here write your first number :"))
 b=int (input("here write your secoond number :"))
 c=a+b
-print("the sum a+b is :", c)
+print("the sum a+b is :", c)ddddd
