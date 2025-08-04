0a8b56ac1de1cdb8e2ec7722c82c996a260f695a
Daniellaananga <daniellasong06.@gmail.com>
2025-08-04 13:04:55 +0100

    conditions_update commiit

--- conditions.py
+++ conditions.py
@@ -1,7 +1,8 @@
+# here we ask the user to enter two numbers
 a=input("Enter first number : ")
 b=input("enter second number : ")
-
+# lets check if the first number a, is greater than the second number b
 if a > b :
-    print("the greatest number is : ", a)
+    print("the greatest number is : ", a) # in case the greatest number is a
 else:
-    print("the greatest number is : ", b)
+    print("the greatest number is : ", b) # in case the greayest number is b
