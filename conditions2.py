3bcd59826f4302e165a7d3a3d4ad84e20b2b84e5
Daniellaananga <daniellasong06.@gmail.com>
2025-08-04 13:57:33 +0100

    conditions2.py commit

---
+++ conditions2.py
@@ -0,0 +1,7 @@
+# lets ask the user age
+age=int(input("Please enter your age : "))
+# we are checking if the user age can permit him or her to be autorised
+if age >= 18:
+    print(" Access Ataurised")
+else:
+    print("Access Denied")
